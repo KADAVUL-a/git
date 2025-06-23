249a34559aec8550dc0afc16e3ce154060023102
DHEEKSHITH.S <dheekshiths.23civil@kongu.edu>
2025-06-23 23:15:55 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,29 @@
+let a=30;
+let b=20;
+let c=a+b;
+let d=a-b;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+let e=d**2;
+let f=a/2;
+console.log(e,f);
+let age=15;
+if(age>18){
+    console.log("yes true")
+
+}
+else{
+    console.log("false")
+}
+if(age>18){
+    console.log("yes true")
+}   
+else if(age<18){
+    console.log("false")
+}
+else{
+    console.log("false")
+}
+
---
+++ thurs.js
@@ -0,0 +1,8 @@
+let a=10;
+let name = "Dheekshith";
+let isbool=true;
+let c;
+let b=null;
+let abc=2345678908765;
+console.log(name)
+
